68620cabe1251455067e3fbaebae314081df0d11
jhdl0157 <jhdl0157@naver.com>
2022-12-03 12:07:33 +0900

    :sparkles: Feat : init project

---
+++ src/main/java/com/example/htmlparsing/HtmlparsingApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlparsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlparsingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlparsingApplication.class, args);
+    }
+
+}
