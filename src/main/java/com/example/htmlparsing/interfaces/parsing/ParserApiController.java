787214148fdeac87e5daebe79d647ec373bc9c8f
jhdl0157 <jhdl0157@naver.com>
2022-12-04 20:11:06 +0900

    :sparkles: Feat : Dto validation 적용

--- src/main/java/com/example/htmlparsing/interfaces/parsing/ParserApiController.java
+++ src/main/java/com/example/htmlparsing/interfaces/parsing/ParserApiController.java
@@ -1,14 +1,15 @@
 package com.example.htmlparsing.interfaces.parsing;
 
-import com.example.htmlparsing.common.CommonResponse;
 import com.example.htmlparsing.common.exception.EntityNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/parsings")
 public class ParserApiController {
     @PostMapping()
-    public void registerHtml(@RequestBody ParsingDto.ParserRequest parserRequest){
+    public void registerHtml(@RequestBody @Valid ParsingDto.ParserRequest parserRequest){
         if(parserRequest!=null){
             throw new EntityNotFoundException();
         }
