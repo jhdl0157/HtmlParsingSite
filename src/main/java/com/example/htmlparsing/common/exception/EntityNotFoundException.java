267d1bc2164587719f5b884684a00469b37e1923
jhdl0157 <jhdl0157@naver.com>
2022-12-04 19:46:47 +0900

    :sparkles: Feat : 에러처리를 위한 Exception 핸들링 클래스 추가

---
+++ src/main/java/com/example/htmlparsing/common/exception/EntityNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.htmlparsing.common.exception;
+
+import com.example.htmlparsing.common.ErrorCode;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
