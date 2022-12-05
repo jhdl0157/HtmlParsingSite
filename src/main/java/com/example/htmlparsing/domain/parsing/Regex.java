f661a4809136dd0e79eb48f3cdd1d633ca59bbad
jhdl0157 <jhdl0157@naver.com>
2022-12-05 10:24:23 +0900

    :sparkles: Feat : 정규식을 담당하는 enum 추가

--- src/main/java/com/example/htmlparsing/domain/parsing/Regex.java
+++ src/main/java/com/example/htmlparsing/domain/parsing/Regex.java
@@ -10,4 +10,15 @@ public enum Regex {
     DELETE_EXCEPT_ALPHA_AND_NUMBER("[^\\p{Alnum}]+"),DELETE_NUMBER("\\p{Digit}+")
     ,EXPECT_NUMBER("[^\\p{Digit}]+");
     private final String regexPattern;
+
+    public static String tagRegex(String html){
+        String deleteTag=html.replaceAll(Regex.DELETE_TAG.getRegexPattern(), "");
+        String deleteMsg=deleteTag.replaceAll(Regex.DELETE_EXCEPT_ALPHA_AND_NUMBER.getRegexPattern(), "");
+        return deleteMsg;
+    }
+    public static String textRegex(String html){
+        String deleteMsg=html.replaceAll(Regex.DELETE_EXCEPT_ALPHA_AND_NUMBER.getRegexPattern(), "");
+        String deleteWhiteSpace=deleteMsg.replaceAll(Regex.WHITE_SPACE.getRegexPattern(),"");
+        return deleteWhiteSpace;
+    }
 }
