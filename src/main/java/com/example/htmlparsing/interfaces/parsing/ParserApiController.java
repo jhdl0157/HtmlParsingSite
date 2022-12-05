bd156040bc8aa1d9f023c996e46b19c87082c4f6
jhdl0157 <jhdl0157@naver.com>
2022-12-05 22:48:43 +0900

    :sparkles: Feat : 임시적으로 ParsingService와 연결하기 추후 리팩토링 할것

--- src/main/java/com/example/htmlparsing/interfaces/parsing/ParserApiController.java
+++ src/main/java/com/example/htmlparsing/interfaces/parsing/ParserApiController.java
@@ -1,18 +1,25 @@
 package com.example.htmlparsing.interfaces.parsing;
 
+import com.example.htmlparsing.common.CommonResponse;
 import com.example.htmlparsing.common.exception.EntityNotFoundException;
+import com.example.htmlparsing.domain.parsing.ParsingService;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/parsings")
+@RequiredArgsConstructor
 public class ParserApiController {
+    private final ParsingService parsingService;
     @PostMapping()
-    public void registerHtml(@RequestBody @Valid ParsingDto.ParserRequest parserRequest){
-        System.out.println(parserRequest.getUrl());
-        System.out.println(parserRequest.getType());
-        System.out.println(parserRequest.getInvide());
+    public CommonResponse registerHtml(@RequestBody @Valid ParsingDto.ParserRequest parserRequest){
+        System.out.println(parserRequest.toString());
+        val result=parsingService.getQuotientAndRemainder(parserRequest.getUrl(),parserRequest.getType(),parserRequest.getInvide());
+        System.out.println(result.toString());
+        return CommonResponse.success(result);
     }
 
 }
