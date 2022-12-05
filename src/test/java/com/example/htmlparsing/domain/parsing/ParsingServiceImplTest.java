90c597a89350bd2133d5cc171a3c8631182d75d6
jhdl0157 <jhdl0157@naver.com>
2022-12-05 11:50:08 +0900

    :white_check_mark: Test : 합쳐지는 경우의수 테스트코드 추가

--- src/test/java/com/example/htmlparsing/domain/parsing/ParsingServiceImplTest.java
+++ src/test/java/com/example/htmlparsing/domain/parsing/ParsingServiceImplTest.java
@@ -11,7 +11,7 @@ class ParsingServiceImplTest {
     @Mock
     JsoupExecutor jsoupExecutor;
 
-    ParsingServiceImpl parsingService=new ParsingServiceImpl(jsoupExecutor );
+    ParsingServiceImpl parsingService=new ParsingServiceImpl(jsoupExecutor);
     @Test
     void 태그제거_테스트() {
         String html="<!DOCTYPE html>\n" +
@@ -56,8 +56,38 @@ class ParsingServiceImplTest {
         System.out.println(parsingResult.getEnglish());
         System.out.println(parsingResult.getNumbers());
         String expect="AAAaaaaaaBBBbbCCCCcccDddddEeeeeeeeeeFghhhhhhhhiiiiiiillllllllmmmmnnOooPpprrrrsssTTTtttttttttttttUYyy";
-        String numExpect="81234";
+        String numExpect="12348";
         assertEquals(expect,parsingResult.getEnglish());
         assertEquals(numExpect,parsingResult.getNumbers());
+    }
+    @Test
+    void 합치기테스트_영어갯수가_숫자갯수보다_많을_경우(){
+        String result=parsingService.combineAlphaAndNumber(new ParsingResult("AaBbCcDdEeFfGg","1234"));
+        String expect="A1a2B3b4CcDdEeFfGg";
+        assertEquals(expect,result);
+    }
+    @Test
+    void 합치기테스트_영어갯수가_숫자갯수보다_같을_경우(){
+        String result=parsingService.combineAlphaAndNumber(new ParsingResult("AaBbCcDdEe","1122334455"));
+        String expect="A1a1B2b2C3c3D4d4E5e5";
+        assertEquals(expect,result);
+    }
+    @Test
+    void 합치기테스트_영어갯수가_숫자갯수보다_적을_경우(){
+        String result=parsingService.combineAlphaAndNumber(new ParsingResult("AaBbCcDdEe","11223344556677"));
+        String expect="A1a1B2b2C3c3D4d4E5e56677";
+        assertEquals(expect,result);
+    }
+    @Test
+    void 합치기테스트_숫자만_존재할_경우(){
+        String result=parsingService.combineAlphaAndNumber(new ParsingResult("","11223344556677"));
+        String expect="11223344556677";
+        assertEquals(expect,result);
+    }
+    @Test
+    void 합치기테스트_영어만_존재할_경우(){
+        String result=parsingService.combineAlphaAndNumber(new ParsingResult("AAABBBBBCCCCDDD",""));
+        String expect="AAABBBBBCCCCDDD";
+        assertEquals(expect,result);
     }
 }
