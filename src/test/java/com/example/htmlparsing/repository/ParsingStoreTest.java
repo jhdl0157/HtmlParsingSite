722dea140386ef251ee070d9cceaa85667eff2ee
jhdl0157 <jhdl0157@naver.com>
2022-12-06 09:57:07 +0900

    :white_check_mark: Test : Repository 기능 테스트 코드 작성

---
+++ src/test/java/com/example/htmlparsing/repository/ParsingStoreTest.java
@@ -0,0 +1,44 @@
+package com.example.htmlparsing.repository;
+
+import com.example.htmlparsing.common.ConvertType;
+import com.example.htmlparsing.domain.parsing.Parsing;
+import com.example.htmlparsing.infrastructure.ParsingRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class ParsingStoreTest {
+
+    @Autowired
+    private ParsingRepository parsingRepository;
+
+    @Test
+    @DisplayName("Parsing 값을 잘 저장 하는지")
+    void t1(){
+        Parsing parsing=new Parsing("test.com", ConvertType.TAG,3,"testQuot","testRemiain");
+        Parsing saveParsing=parsingRepository.save(parsing);
+
+        Assertions.assertThat(parsing).isSameAs(saveParsing);
+        Assertions.assertThat(parsing.getUrl()).isEqualTo(saveParsing.getUrl());
+        Assertions.assertThat(parsing.getId()).isNotNull();
+    }
+    @Test
+    @DisplayName("find기능이 제대로 동작하는지 확인")
+    void t2(){
+        Parsing parsing=new Parsing("test.com", ConvertType.TAG,3,"testQuot","testRemiain");
+        parsingRepository.save(parsing);
+
+        Parsing findParsing=parsingRepository.findById(1L).orElseThrow(()-> new IllegalArgumentException("값을 찾지 못했습니다."));
+        Assertions.assertThat(parsingRepository.count()).isEqualTo(1);
+        Assertions.assertThat(findParsing.getUrl()).isEqualTo("test.com");
+        Assertions.assertThat(findParsing.getInvide()).isEqualTo(3);
+        Assertions.assertThat(findParsing.getQuotient()).isEqualTo("testQuot");
+        Assertions.assertThat(findParsing.getRemainder()).isEqualTo("testRemiain");
+    }
+
+}
